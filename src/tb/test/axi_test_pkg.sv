0cfc18b56650b9b11fee9683c8e55304e9154a92
Akshay J Rai <107136628+axyrai@users.noreply.github.com>
2024-01-29 10:39:53 +0530

    Update axi_test_pkg.sv

--- src/tb/test/axi_test_pkg.sv
+++ src/tb/test/axi_test_pkg.sv
@@ -4,4 +4,14 @@ package axi_master_pkg;
  import axi_env_pkg::*;
  import axi_master_pkg::*;
  import axi_sequence_pkg::*;
+
+ `include "axi_base_test.sv"
+ `include "axi_master_random_test.sv"
+ `include "axi_master_read_16b_test.sv"
+ `include "axi_master_read_32b_test.sv"
+ `include "axi_master_read_8b_test.sv"
+ `include "axi_master_write_16b_test.sv"
+ `include "axi_master_write_8b_test.sv"
+ `include "axi_master_write_32b_test.sv"
+
 endpackage
