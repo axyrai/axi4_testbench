344e934ea31b8de585e85948f548795b5b885b04
Akshay J Rai <107136628+axyrai@users.noreply.github.com>
2024-01-29 10:12:54 +0530

    Update axi_master_pkg.sv

--- src/tb/master/axi_master_pkg.sv
+++ src/tb/master/axi_master_pkg.sv
@@ -1,6 +1,6 @@
 package axi_master_pkg;
   `include "uvm_macros.svh"
-  import uvm_pkg::*;
   `include "../../globals/axi_globals_pkg.sv"
-import axi_globals_pkg::*;
+  import uvm_pkg::*;
+  import axi_globals_pkg::*;
 endpackage
