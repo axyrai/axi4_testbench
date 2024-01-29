3508de92709c00004e333fd2b65a72c1e46e4b8d
Akshay J Rai <107136628+axyrai@users.noreply.github.com>
2024-01-29 10:43:12 +0530

    Update axi_sequence_pkg.sv

--- src/tb/test/sequences/axi_sequence_pkg.sv
+++ src/tb/test/sequences/axi_sequence_pkg.sv
@@ -1,4 +1,13 @@
 package axi_sequence_pkg;
-`include "uvm_macros.svh"
-import uvm_pkg::*;
+  `include "uvm_macros.svh"
+  import uvm_pkg::*;
+
+  `include "axi_master_base_sequence.sv"
+  `include "axi_master_random_sequence.sv"
+  `include "axi_master_read_8b_sequence.sv"
+  `include "axi_master_read_16b_sequence.sv"
+  `include "axi_master_read_32b_sequence.sv"
+  `include "axi_master_write_8b_sequence.sv"
+  `include "axi_master_write_16b_sequence.sv"
+  `include "axi_master_write_32b_sequence.sv"
 endpackage  
