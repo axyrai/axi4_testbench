b54c0bceeaeb4c4e38b1964dfa1acdb55b6170f6
Akshay J Rai <107136628+axyrai@users.noreply.github.com>
2024-01-29 11:07:21 +0530

    Update axi_env_pkg.sv

--- src/tb/env/axi_env_pkg.sv
+++ src/tb/env/axi_env_pkg.sv
@@ -3,7 +3,7 @@ package axi_env_pkg;
   import uvm_pkg::*;
   import axi4_globals_pkg::*;
   import axi_master_pkg::*;
-
-  `include "axi_master_environment.sv"
   `include "axi_master_scoreboard.sv"
+  `include "axi_master_environment.sv"
+
 endpackage
