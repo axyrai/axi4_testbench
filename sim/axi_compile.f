0485f29d893747b0dc9340871d6dc5fe87730b20
Akshay J Rai <107136628+axyrai@users.noreply.github.com>
2024-01-29 11:25:07 +0530

    Update axi_compile.f

--- sim/axi_compile.f
+++ sim/axi_compile.f
@@ -8,6 +8,11 @@
 +incdir+../../src/hvl_top/test
 ../../src/dut/axi_ram.v
 
+../../src/tb/master/axi_master_pkg.sv
+../../src/tb/env/axi_env_pkg.sv
+../../src/globals/axi4_globals_pkg.sv
+../../src/tb/test/axi_test_pkg.sv
+
 ../../src/tb/axi_if/axi_master_interface.sv
 ../../src/tb/master/axi_master_transaction.sv
 ../../src/tb/master/axi_master_driver.sv
@@ -21,10 +26,7 @@
 
 
 
-../../src/tb/master/axi_master_pkg.sv
-../../src/tb/env/axi_env_pkg.sv
-../../src/globals/axi4_globals_pkg.sv
-../../src/tb/test/axi_test_pkg.sv
+
 ../../src/tb/axi_master_top.sv
 
 
