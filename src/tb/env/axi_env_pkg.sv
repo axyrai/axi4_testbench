57c632d28dc219d7fb21741fa5aaac427c1ef8ac
Akshay J Rai <107136628+axyrai@users.noreply.github.com>
2024-01-29 10:14:12 +0530

    Update axi_env_pkg.sv

--- src/tb/env/axi_env_pkg.sv
+++ src/tb/env/axi_env_pkg.sv
@@ -1,8 +1,7 @@
 package axi_env_pkg;
 `include "uvm_macros.svh"
-import uvm_pkg::*;
-    `include "../master/axi_master_transaction.sv"
+`include "../master/axi_master_transaction.sv"
 `include "axi_master_scoreboard.sv"
 `include "../master/axi_master_agent.sv"
-
+import uvm_pkg::*;
 endpackage
