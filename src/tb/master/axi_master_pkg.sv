94a62af44956c3f60079ffd2aa3d31919c264e44
Akshay J Rai <107136628+axyrai@users.noreply.github.com>
2024-01-29 10:25:23 +0530

    Update axi_master_pkg.sv

--- src/tb/master/axi_master_pkg.sv
+++ src/tb/master/axi_master_pkg.sv
@@ -3,4 +3,10 @@ package axi_master_pkg;
  // `include "../../globals/axi_globals_pkg.sv"
   import uvm_pkg::*;
   import axi_globals_pkg::*;
+
+  `include "axi_master_agent.sv"
+  `include "axi_master_driver.sv"
+  `include "axi_master_monitor.sv"
+  `include "axi_master_sequencer.sv"
+  `include "axi_master_transaction.sv"
 endpackage
