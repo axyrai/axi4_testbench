29150ab2a4f7f1770eebfecc5b346d41fb0d142f
Akshay J Rai <107136628+axyrai@users.noreply.github.com>
2024-01-29 11:13:00 +0530

    Update axi_master_pkg.sv

--- src/tb/master/axi_master_pkg.sv
+++ src/tb/master/axi_master_pkg.sv
@@ -2,7 +2,7 @@ package axi_master_pkg;
   `include "uvm_macros.svh"
  // `include "../../globals/axi_globals_pkg.sv"
   import uvm_pkg::*;
-  import axi_globals_pkg::*;
+  import axi4_globals_pkg::*;
 
   `include "axi_master_agent.sv"
   `include "axi_master_driver.sv"
