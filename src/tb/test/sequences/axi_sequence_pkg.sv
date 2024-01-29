b4384b5f31e37a8fea68ddb92a407e4811878bd2
Akshay J Rai <107136628+axyrai@users.noreply.github.com>
2024-01-29 12:14:09 +0530

    Update axi_sequence_pkg.sv

--- src/tb/test/sequences/axi_sequence_pkg.sv
+++ src/tb/test/sequences/axi_sequence_pkg.sv
@@ -6,10 +6,10 @@ package axi_sequence_pkg;
 
   `include "axi_master_base_sequence.sv"
   `include "axi_master_random_sequence.sv"
-  `include "axi_master_read_8b_sequence.sv"
-  `include "axi_master_read_16b_sequence.sv"
-  `include "axi_master_read_32b_sequence.sv"
-  `include "axi_master_write_8b_sequence.sv"
-  `include "axi_master_write_16b_sequence.sv"
-  `include "axi_master_write_32b_sequence.sv"
+  `include "axi_master_read_8b_transfer.sv"
+  `include "axi_master_read_16b_transfer.sv"
+  `include "axi_master_read_32b_transfer.sv"
+  `include "axi_master_write_8b_transfer.sv"
+  `include "axi_master_write_16b_transfer.sv"
+  `include "axi_master_write_32b_transfer.sv"
 endpackage  
